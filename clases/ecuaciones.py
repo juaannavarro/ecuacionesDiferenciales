a1d46f8e781d18fc96be3d5f8bfcba9a478c88d6
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-10-13 20:41:55 +0200

    Update ecuaciones.py

--- clases/ecuaciones.py
+++ clases/ecuaciones.py
@@ -14,7 +14,7 @@ def ec1():
         sympy.dsolve(y(x).diff(x) - f, ics={y(3): -1})
         print(sympy.dsolve(y(x).diff(x) - f))
         print(sympy.dsolve(y(x).diff(x) - f, ics={y(3): -1}))
-ec1()
+
 def ec2():
         global x , y 
         print("ECUACIÓN 2")
@@ -23,14 +23,13 @@ def ec2():
         sympy.dsolve(y(x).diff(x) - f1, ics={y(math.pi/2): math.e})
         print(sympy.dsolve(y(x).diff(x) - f1))
         print(sympy.dsolve(y(x).diff(x) - f1, ics={y(math.pi/2): math.e}))
-ec2()
+
 def ec3():
         global x, y
         print("ECUACIÓN 3")
         f2 = 2*(x-2)**2 + y(x)/(x-2)
         sympy.dsolve(y(x).diff(x) - f2)
         print(sympy.dsolve(y(x).diff(x) - f2))
-ec3()
 def ec4():
         print("ECUACIÓN 4")
         f3 = (3*x**2)+y(x)/(x*2)
@@ -39,4 +38,4 @@ def ec4():
         f4 = (y(x)+3*x**2)/2*x
         p = sympy.dsolve(y(x).diff(x) - f4)
         print("Familia de soluciones generales de la ecuación diferencial y'= (y+3x^2) / 2x", p )
-ec4()
+
